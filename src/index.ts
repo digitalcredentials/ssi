00269cd1668e751faf60edd7c6a5ae36f70c8708
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-04-29 16:37:09 -0400

    Update changelog.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
+ * Copyright (c) 2025 Digital Credentials Consortium. All rights reserved.
  */
 export * from './LD'
 export * from './DID'
